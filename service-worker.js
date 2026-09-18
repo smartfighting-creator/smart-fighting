ac07b22feb03296277fc3edd97ee55d8f8ab5afb
smartfighting-creator <smartfighting@libero.it>
2026-09-18 06:53:27 +0200

    Add PWA service worker

---
+++ service-worker.js
@@ -0,0 +1,14 @@
+const CACHE='smart-fighting-pwa-v2-20260918';
+const SHELL=['./','./index.html','./manifest.webmanifest','./icon.svg','./offline.html'];
+self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)))});
+self.addEventListener('activate',event=>{event.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))]))});
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  const url=new URL(event.request.url);
+  if(url.origin!==self.location.origin)return;
+  if(event.request.mode==='navigate'){
+    event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(async()=>await caches.match(event.request)||await caches.match('./index.html')||await caches.match('./offline.html')));
+    return;
+  }
+  event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request).then(response=>{if(response.ok){const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy))}return response}).catch(()=>caches.match('./offline.html'))));
+});
